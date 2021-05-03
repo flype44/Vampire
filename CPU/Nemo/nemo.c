588883b2f867889c7fc2da144cdae08b2932d7a2
flype44 <flype44@gmail.com>
2021-05-03 20:13:08 +0200

    Added TEMPLATE/N argument
    Colored result in the CLI.

--- CPU/Nemo/nemo.c
+++ CPU/Nemo/nemo.c
@@ -24,6 +24,14 @@
  * 
  *****************************************************************
  * 
+ * Arguments:
+ * 
+ * T=TEMPLATE/N:   Color scheme template. 0 (default) to 2.
+ * 
+ * M=MNEMONIC/A/F: Assembly mnemonic to encode (mandatory).
+ * 
+ *****************************************************************
+ * 
  * Examples:
  * 
  * > nemo nop
@@ -67,9 +75,14 @@
 #include <exec/types.h>
 #include <exec/memory.h>
 #include <exec/execbase.h>
+#include <proto/exec.h>
+
 #include <dos/dos.h>
 #include <proto/dos.h>
-#include <proto/exec.h>
+
+#include <libraries/disassembler.h>
+#include <proto/disassembler.h>
+
 #include "nemo.h"
 
 /*****************************************************************
@@ -78,27 +91,154 @@
  * 
  *****************************************************************/
 
+#define COLOR0    "\033[30m" /* Color Grey  */
+#define COLOR1    "\033[31m" /* Color Black */
+#define COLOR2    "\033[32m" /* Color White */
+#define COLOR3    "\033[33m" /* Color Blue  */
+#define COLOR4    "\033[34m" /* Color #4    */
+#define COLOR5    "\033[35m" /* Color #5    */
+#define COLOR6    "\033[36m" /* Color #6    */
+#define COLOR7    "\033[37m" /* Color #7    */
+
 #define FILE_ASM  "T:NEMO.ASM"
 #define FILE_OUT  "T:NEMO.OUT"
 #define FILE_EXE  "vasmm68k_mot_os3"
-#define FILE_CMD  "%s -no-opt -Fbin -m68080 -m68882 %s -o %s -quiet"
+#define FILE_CMD  "%s -Fbin -no-opt -m68080 -m68882 %s -o %s -quiet"
 
-#define TEMPLATE  "MNEMONIC/A/F"
+#define TEMPLATE  "M=MNEMONIC/A/F,T=TEMPLATE/N"
 
-#define OPT_MNEMONIC  0
-#define OPT_COUNT     1
+/*****************************************************************
+ * 
+ * ENUMS
+ * 
+ *****************************************************************/
+
+enum {
+	OPT_MNEMONIC,
+	OPT_TEMPLATE,
+	OPT_COUNT
+};
+
+enum {
+	FIELD_OPEN,
+	FIELD_HEX,
+	FIELD_BIN,
+	FIELD_CLOSE,
+	FIELD_STAT,
+	FIELD_COUNT
+};
+
+enum {
+	TEMPLATE_0,
+	TEMPLATE_1,
+	TEMPLATE_2,
+	TEMPLATE_COUNT
+};
+
+/*****************************************************************
+ * 
+ * TEMPLATES
+ * 
+ *****************************************************************/
+
+const STRPTR templates[TEMPLATE_COUNT][FIELD_COUNT] =
+{
+	///////////////////////////////////
+	//
+	// TEMPLATE #1
+	//
+	///////////////////////////////////
+	
+	{
+		"\nOPCODE MACRO\n\t* %s\n",
+		"\tDC.W $%04x",
+		" ; %s %s %s %s\n",
+		"\tENDM\n",
+		"\nOpcode-length: %ld bytes.\n",
+	},
+	
+	///////////////////////////////////
+	//
+	// TEMPLATE #2
+	//
+	///////////////////////////////////
+	
+	{
+		"\n" COLOR2 "OPCODE " COLOR3 "MACRO\n" COLOR0 "\t* %s\n",
+		"\t" COLOR2 "DC" COLOR2 ".W " COLOR2 "$%04x",
+		COLOR0 " ; %s %s %s %s\n",
+		COLOR3 "\tENDM\n",
+		"\n" COLOR2 "Opcode-length: " COLOR3 "%ld bytes.\n",
+	},
+	
+	///////////////////////////////////
+	//
+	// TEMPLATE #3
+	//
+	///////////////////////////////////
+	
+	{
+		"\n" COLOR2 "OPCODE " COLOR7 "MACRO\n" COLOR3 "\t* %s\n",
+		"\t" COLOR2 "DC" COLOR2 ".W " COLOR6 "$%04x",
+		COLOR0 " ; %s %s %s %s\n",
+		COLOR7 "\tENDM\n",
+		"\n" COLOR2 "Opcode-length: " COLOR3 "%ld bytes.\n",
+	}
+};
+
+/*****************************************************************
+ * 
+ * PutChProc()
+ * 
+ *****************************************************************/
+
+UWORD PutChProc[] =
+{ 
+	0x16C0, // move.b d0,(a3)+
+	0x4E75  // rts
+};
+
+/*****************************************************************
+ * 
+ * GLOBALS
+ * 
+ *****************************************************************/
+
+ULONG tmplt = 0;
 
 STRPTR appName = APP_NAME;
 STRPTR appVers = APP_VSTRING;
 
+extern struct DOSBase  *DOSBase;
+extern struct ExecBase *SysBase;
+
+struct Library *DisassemblerBase;
+
 /*****************************************************************
  * 
- * EXTERNS
+ * VSPrintf()
  * 
  *****************************************************************/
 
-extern struct DOSBase  *DOSBase;
-extern struct ExecBase *SysBase;
+VOID VSPrintf(STRPTR str, STRPTR fmt, APTR args)
+{
+	RawDoFmt(fmt, args, (VOID (*)(VOID))&PutChProc, str);
+}
+
+/*****************************************************************
+ * 
+ * PrintHex()
+ * 
+ *****************************************************************/
+
+VOID PrintHex(UWORD n)
+{
+	UWORD argv[1];
+	
+	argv[0] = n;
+	
+	VPrintf(templates[tmplt][FIELD_HEX], argv);
+}
 
 /*****************************************************************
  * 
@@ -123,39 +263,22 @@ VOID PrintBin(UWORD n)
 	argv[2] = nibble[(n >>  4) & 15];
 	argv[3] = nibble[(n >>  0) & 15];
 	
-	VPrintf("%s %s %s %s\n", argv);
+	VPrintf(templates[tmplt][FIELD_BIN], argv);
 }
 
 /*****************************************************************
  * 
- * PrintHex()
+ * PrintInt()
  * 
  *****************************************************************/
 
-VOID PrintHex(UWORD n)
+VOID PrintInt(STRPTR s, ULONG n)
 {
-	UWORD argv[1];
+	ULONG argv[1];
 	
 	argv[0] = n;
 	
-	VPrintf("\tDC.W $%04x ; ", argv);
-}
-
-/*****************************************************************
- * 
- * VSPrintf()
- * 
- *****************************************************************/
-
-VOID VSPrintf(STRPTR str, STRPTR fmt, APTR args)
-{
-	const UWORD PutChProc[] =
-	{ 
-		0x16C0, // move.b d0,(a3)+
-		0x4E75  // rts
-	};
-	
-	RawDoFmt(fmt, args, (VOID (*)(VOID))&PutChProc, str);
+	VPrintf(s, &argv);
 }
 
 /*****************************************************************
@@ -170,55 +293,94 @@ ULONG main(ULONG argc, STRPTR argv[])
 	LONG opts[OPT_COUNT];  
 	struct RDArgs *rdargs;   
 	
-	opts[0] = NULL;
+	opts[0] = 0;
+	opts[1] = 0;
 	
 	if (rdargs = ReadArgs(TEMPLATE, opts, NULL))
 	{
-		BPTR file;
+		tmplt = *(ULONG *)opts[OPT_TEMPLATE];
 		
-		if (file = Open(FILE_ASM, MODE_NEWFILE))
+		if (tmplt < TEMPLATE_COUNT)
 		{
-			BYTE commandString[255];
+			BPTR file;
 			
-			STRPTR commandArgs[] = {
-				FILE_EXE,
-				FILE_ASM,
-				FILE_OUT
-			};
-			
-			FPuts(file, "MAIN:\n\t");
-			FPuts(file, (STRPTR)opts[OPT_MNEMONIC]);
-			Close(file);
-			
-			VSPrintf(commandString, FILE_CMD, commandArgs);
-			
-			if (Execute(commandString, NULL, NULL))
+			if (file = Open(FILE_ASM, MODE_NEWFILE))
 			{
-				if (file = Open(FILE_OUT, MODE_OLDFILE))
-				{
-					UWORD opcode;
-					
-					VPrintf("\n\t; %s\n", &opts[OPT_MNEMONIC]);
-					
-					while (Read(file, &opcode, sizeof(UWORD)) > 0)
-					{
-						PrintHex(opcode);
-						PrintBin(opcode);
-					}
-					
-					rc = RETURN_OK;
-					
-					Close(file);
-					
-					DeleteFile(FILE_OUT);
-				}
-			}
+				BYTE commandString[255];
+				
+				STRPTR commandArgs[] = {
+					FILE_EXE,
+					FILE_ASM,
+					FILE_OUT
+				};
 			
-			DeleteFile(FILE_ASM);
+				FPuts(file, "MAIN:\n\t");
+				FPuts(file, (STRPTR)opts[OPT_MNEMONIC]);
+				Close(file);
+				
+				VSPrintf(commandString, FILE_CMD, commandArgs);
+				
+				if (Execute(commandString, NULL, NULL))
+				{
+					if (file = Open(FILE_OUT, MODE_OLDFILE))
+					{
+						UWORD opcode;
+						ULONG count = 0;
+						UWORD opcodes[32];
+						
+						VPrintf(templates[tmplt][FIELD_OPEN], 
+							&opts[OPT_MNEMONIC]);
+						
+						while (Read(file, &opcode, sizeof(UWORD)) > 0)
+						{
+							opcodes[count] = opcode;
+							PrintHex(opcode);
+							PrintBin(opcode);
+							count++;
+						}
+						
+						PutStr(templates[tmplt][FIELD_CLOSE]);
+						
+						PrintInt(templates[tmplt][FIELD_STAT], count * 2);
+						
+						/*
+						if (DisassemblerBase = OpenLibrary(DISASSEMBLER_NAME, 40))
+						{
+							struct DisData ds;
+							
+							ds.ds_PutProc  = (void *)&PutChProc;
+							ds.ds_PC       = &opcodes;
+							ds.ds_From     = &opcodes;
+							ds.ds_UpTo     = (APTR)(((ULONG)&opcodes) + (count * 2));
+							ds.ds_UserData = NULL;
+							ds.ds_UserBase = NULL;
+							ds.ds_Truncate = 0;
+							ds.ds_reserved = 0;
+							
+							Disassemble(&ds);
+							
+							CloseLibrary(DisassemblerBase);
+						}
+						*/
+						
+						rc = RETURN_OK;
+						
+						Close(file);
+						
+						DeleteFile(FILE_OUT);
+					}
+				}
+				
+				DeleteFile(FILE_ASM);
+			}
+			else
+			{
+				PrintFault(IoErr(), appName);
+			}
 		}
 		else
 		{
-			PrintFault(IoErr(), appName);
+			PutStr("Invalid template! 0=B&W (default), 1=Colored\n");
 		}
 		
 		FreeArgs(rdargs);
--- CPU/Nemo/nemo.h
+++ CPU/Nemo/nemo.h
@@ -2,6 +2,6 @@
 #define __NEMO_H__
 
 #define APP_NAME    "Nemo"
-#define APP_VSTRING "$VER: Nemo 0.4 (24.4.2021) APOLLO-Team"
+#define APP_VSTRING "$VER: Nemo 0.5 (3.5.2021) APOLLO-Team"
 
 #endif
